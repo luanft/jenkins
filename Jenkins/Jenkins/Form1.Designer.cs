9043a3bb0bcc62aad60484f9f7f05f9aa2a09246
luan <xcrossworker@gmail.com>
2017-02-15 10:54:51 +0700

    Hehe

--- Jenkins/Jenkins/Form1.Designer.cs
+++ Jenkins/Jenkins/Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Test = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_Test
             // 
-            this.button1.Location = new System.Drawing.Point(239, 200);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_Test.Location = new System.Drawing.Point(239, 200);
+            this.btn_Test.Name = "btn_Test";
+            this.btn_Test.Size = new System.Drawing.Size(75, 23);
+            this.btn_Test.TabIndex = 0;
+            this.btn_Test.Text = "button1";
+            this.btn_Test.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 262);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Test);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +54,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Test;
     }
 }
 
